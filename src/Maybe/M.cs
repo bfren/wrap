acd4075a34f44247de5028ffe055b016b56faeff
bfren <bf@bfren.dev>
2023-08-11 13:15:17 +0100

    Adding DefaultCulture property

--- src/Maybe/M.cs
+++ src/Maybe/M.cs
@@ -1,9 +1,17 @@
 // Monadic: .NET monads for functional style.
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
 
+using System.Globalization;
+
 namespace Monadic;
 
 /// <summary>
 /// Pure functions for interacting with <see cref="Maybe{T}"/> types.
 /// </summary>
-public static partial class M { }
+public static partial class M
+{
+	/// <summary>
+	/// Default culture (en-GB) - used when parsing strings
+	/// </summary>
+	public static CultureInfo DefaultCulture { get; set; } = CultureInfo.GetCultureInfo("en-GB");
+}
