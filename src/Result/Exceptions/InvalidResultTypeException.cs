00136179f2b9d294314ff5523fae0575e3f03519
bfren <bf@bfren.dev>
2023-08-22 08:08:27 +0100

    Adding Exception comments

--- src/Result/Exceptions/InvalidResultTypeException.cs
+++ src/Result/Exceptions/InvalidResultTypeException.cs
@@ -5,7 +5,13 @@ using System;
 
 namespace Monadic.Exceptions;
 
+/// <summary>
+/// Thrown when a switch function encounters an <see cref="Result{T}"/> type that is
+/// neither <see cref="Ok{T}"/> nor <see cref="Err"/>.
+/// </summary>
 public sealed class InvalidResultTypeException : InvalidTypeException
 {
+	/// <summary>Create exception.</summary>
+	/// <param name="type">The invalid type that has been encountered.</param>
 	internal InvalidResultTypeException(Type type) : base(type, typeof(Result<>), typeof(Err), typeof(Ok<>)) { }
 }
--- src/Result/Exceptions/SimpleErrorException.cs
+++ src/Result/Exceptions/SimpleErrorException.cs
@@ -5,9 +5,21 @@ using System;
 
 namespace Monadic.Exceptions;
 
+/// <summary>
+/// Used to wrap a simple error message as an exception.
+/// </summary>
+/// <seealso cref="R.Err(string)"/>
+/// <seealso cref="R.Err{T}(string)"/>
 public sealed class SimpleErrorException : Exception
 {
+	/// <inheritdoc cref="SimpleErrorException(string, Exception)"/>
 	public SimpleErrorException() { }
+
+	/// <inheritdoc cref="SimpleErrorException(string, Exception)"/>
 	public SimpleErrorException(string message) : base(message) { }
+
+	/// <summary>Create exception.</summary>
+	/// <param name="message">Error message.</param>
+	/// <param name="inner">Inner exception.</param>
 	public SimpleErrorException(string message, Exception inner) : base(message, inner) { }
 }
