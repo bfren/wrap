2b00d9a52f76ce97d1676edbacb81da448c83a32
bfren <bf@bfren.dev>
2023-09-03 08:39:19 +0100

    Adding IsFalse and IsTrue

---
+++ src/Maybe/MaybeExtensions.IsFalse.cs
@@ -0,0 +1,28 @@
+// Monadic: .NET monads for functional style.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+using System.Threading.Tasks;
+
+namespace Monadic;
+
+public static partial class MaybeExtensions
+{
+	/// <summary>
+	/// Returns <see langword="false"/> if <paramref name="this"/> is <see cref="None"/>,
+	/// or the value of <paramref name="this"/>.
+	/// </summary>
+	/// <param name="this">Maybe object.</param>
+	/// <returns>Whether or not the value of <paramref name="this"/> is <see langword="false"/>.</returns>
+	public static bool IsFalse(this Maybe<bool> @this) =>
+		M.Switch(@this,
+			none: false,
+			some: x => x == false
+		);
+
+	/// <inheritdoc cref="IsTrue(Maybe{bool})"/>
+	public static Task<bool> IsFalseAsync(this Task<Maybe<bool>> @this) =>
+		M.SwitchAsync(@this,
+			none: false,
+			some: x => x
+		);
+}
---
+++ src/Maybe/MaybeExtensions.IsTrue.cs
@@ -0,0 +1,28 @@
+// Monadic: .NET monads for functional style.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+using System.Threading.Tasks;
+
+namespace Monadic;
+
+public static partial class MaybeExtensions
+{
+	/// <summary>
+	/// Returns <see langword="false"/> if <paramref name="this"/> is <see cref="None"/>,
+	/// or the value of <paramref name="this"/>.
+	/// </summary>
+	/// <param name="this">Maybe object.</param>
+	/// <returns>Whether or not the value of <paramref name="this"/> is <see langword="true"/>.</returns>
+	public static bool IsTrue(this Maybe<bool> @this) =>
+		M.Switch(@this,
+			none: false,
+			some: x => x
+		);
+
+	/// <inheritdoc cref="IsTrue(Maybe{bool})"/>
+	public static Task<bool> IsTrueAsync(this Task<Maybe<bool>> @this) =>
+		M.SwitchAsync(@this,
+			none: false,
+			some: x => x
+		);
+}
