9727806a316c3b9b1c66aa5d8bf8fa0c69b51e63
bfren <bf@bfren.dev>
2023-09-22 12:40:36 +0100

    Using Try when Mapping

--- src/Result/ResultExtensions.Map.cs
+++ src/Result/ResultExtensions.Map.cs
@@ -11,24 +11,24 @@ public static partial class ResultExtensions
 	public static Result<TReturn> Map<T, TReturn>(this Result<T> @this, Func<T, TReturn> map) =>
 		R.Switch(@this,
 			err: R.Err<TReturn>,
-			ok: x => R.Wrap(map(x))
+			ok: x => R.Try(() => map(x))
 		);
 
 	public static Task<Result<TReturn>> MapAsync<T, TReturn>(this Result<T> @this, Func<T, Task<TReturn>> map) =>
 		R.SwitchAsync(@this,
 			err: R.Err<TReturn>,
-			ok: async x => R.Wrap(await map(x))
+			ok: x => R.TryAsync(() => map(x))
 		);
 
 	public static Task<Result<TReturn>> MapAsync<T, TReturn>(this Task<Result<T>> @this, Func<T, TReturn> map) =>
 		R.SwitchAsync(@this,
 			err: R.Err<TReturn>,
-			ok: x => R.Wrap(map(x))
+			ok: x => R.Try(() => map(x))
 		);
 
 	public static Task<Result<TReturn>> MapAsync<T, TReturn>(this Task<Result<T>> @this, Func<T, Task<TReturn>> map) =>
 		R.SwitchAsync(@this,
 			err: R.Err<TReturn>,
-			ok: async x => R.Wrap(await map(x))
+			ok: x => R.TryAsync(() => map(x))
 		);
 }
