ae6055f439b63f36be4ef4d6a035424a8bd68504
bfren <bf@bfren.dev>
2024-03-27 16:57:05 +0000

    Adding Unsafe extension method

---
+++ src/Result/ResultExtensions.Unsafe.cs
@@ -0,0 +1,21 @@
+// Wrap: .NET monads for functional style.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+using System.Threading.Tasks;
+
+namespace Wrap;
+
+public static partial class ResultExtensions
+{
+	/// <summary>
+	/// Wraps <paramref name="this"/> in an <see cref="Unsafe{T}(Result{T})"/> to enable unsafe functions.
+	/// </summary>
+	/// <param name="this">Result object.</param>
+	/// <returns>Unsafe wrapper containing <paramref name="this"/>.</returns>
+	public static Unsafe<T> Unsafe<T>(this Result<T> @this) =>
+		new() { Result = @this };
+
+	/// <inheritdoc cref="Unsafe{T}(Result{T})"/>
+	public static async Task<Unsafe<T>> Unsafe<T>(this Task<Result<T>> @this) =>
+		new() { Result = await @this };
+}
