679c05ff21982d85a0b15492f2ade80631c16484
bfren <bf@bfren.dev>
2026-01-20 15:46:49 +0000

    Adding Ifxxx methods

---
+++ src/Maybe/MaybeExtensions.ContinueIf.cs
@@ -0,0 +1,25 @@
+// Wrap: .NET monads for functional style.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+using System;
+using System.Threading.Tasks;
+
+namespace Wrap;
+
+public static partial class MaybeExtensions
+{
+	/// <summary>
+	/// Return a value determined by the value of <paramref name="this"/> and the result of <paramref name="predicate"/>.
+	/// </summary>
+	/// <typeparam name="T">Some value type.</typeparam>
+	/// <param name="this">Maybe object.</param>
+	/// <param name="predicate">Uses value of <paramref name="this"/> to determines whether to pass on the original value or return <see cref="None"/>.</param>
+	/// <returns>Original value or <see cref="None"/>.</returns>
+	public static Maybe<T> ContinueIf<T>(this Maybe<T> @this, Func<T, bool> predicate) =>
+		If<T, T>(@this, predicate, x => x, _ => M.None);
+
+	/// <inheritdoc cref="ContinueIf{T}(Maybe{T}, Func{T, bool})"/>
+	public static Task<Maybe<T>> ContinueIfAsync<T>(this Task<Maybe<T>> @this, Func<T, bool> predicate) =>
+		IfAsync<T, T>(@this, predicate, x => x, _ => M.None);
+
+}
