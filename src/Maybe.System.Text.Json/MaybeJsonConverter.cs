3639d1ae2bce24367b6451f9de26423951c55492
bfren <bf@bfren.dev>
2026-01-20 15:54:47 +0000

    Internal creation only for JSON converter

--- src/Maybe.System.Text.Json/MaybeJsonConverter.cs
+++ src/Maybe.System.Text.Json/MaybeJsonConverter.cs
@@ -14,11 +14,6 @@ namespace Wrap.Json;
 public sealed class MaybeJsonConverter<T> : JsonConverter<Maybe<T>>
 {
 	/// <summary>
-	/// Internal creation only.
-	/// </summary>
-	internal MaybeJsonConverter() { }
-
-	/// <summary>
 	/// Read value and return as <see cref="Some{T}"/> or <see cref="None"/>.
 	/// </summary>
 	/// <param name="reader">Utf8JsonReader.</param>
