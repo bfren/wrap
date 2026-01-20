7b3298f903e56f4306edd9f8bfc6dd760a4273d6
bfren <bf@bfren.dev>
2026-01-20 15:53:59 +0000

    Adding None AsTask method

--- src/Maybe/Functions/M.None.cs
+++ src/Maybe/Functions/M.None.cs
@@ -1,6 +1,8 @@
 // Wrap: .NET monads for functional style.
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
 
+using System.Threading.Tasks;
+
 namespace Wrap;
 
 public static partial class M
@@ -8,6 +10,15 @@ public static partial class M
 	/// <summary>
 	/// Create a new <see cref="Wrap.None"/> value.
 	/// </summary>
+	/// <returns>None value.</returns>
 	public static None None =>
 		new();
+
+	/// <summary>
+	/// Create a new <see cref="Wrap.None"/> value wrapped as a Task.
+	/// </summary>
+	/// <typeparam name="T">Some value type.</typeparam>
+	/// <returns>None value wrapped as a Task.</returns>
+	public static Task<Maybe<T>> NoneAsTask<T>() =>
+		Maybe<T>.None.Create().AsTask();
 }
