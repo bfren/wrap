64ffecec9e9e97136b3482ccd267fc6c70c7bc09
bfren <bf@bfren.dev>
2026-01-27 14:09:48 +0000

    Making E.Match generic

--- src/Either/Extensions/EitherExtensions.Match.cs
+++ src/Either/Extensions/EitherExtensions.Match.cs
@@ -8,47 +8,58 @@ namespace Wrap.Extensions;
 
 public static partial class EitherExtensions
 {
-	/// <inheritdoc cref="E.Match{TLeft, TRight}(Either{TLeft, TRight}, Action{TLeft}, Action{TRight})"/>
-	public static void Match<TLeft, TRight>(this Either<TLeft, TRight> @this, Action<TLeft> left, Action<TRight> right) =>
+	/// <inheritdoc cref="E.Match{TEither, TLeft, TRight}(TEither, Action{TLeft}, Action{TRight})"/>
+	public static void Match<TEither, TLeft, TRight>(this TEither @this, Action<TLeft> left, Action<TRight> right)
+		where TEither : IEither<TEither, TLeft, TRight> =>
 		E.Match(@this, left, right);
 
-	/// <inheritdoc cref="E.Match{TLeft, TRight}(Either{TLeft, TRight}, Action{TLeft}, Action{TRight})"/>
-	public static Task MatchAsync<TLeft, TRight>(this Either<TLeft, TRight> @this, Func<TLeft, Task> left, Func<TRight, Task> right) =>
+	/// <inheritdoc cref="E.Match{TEither, TLeft, TRight}(TEither, Action{TLeft}, Action{TRight})"/>
+	public static Task MatchAsync<TEither, TLeft, TRight>(this TEither @this, Func<TLeft, Task> left, Func<TRight, Task> right)
+		where TEither : IEither<TEither, TLeft, TRight> =>
 		E.MatchAsync(@this, left, right);
 
-	/// <inheritdoc cref="E.Match{TLeft, TRight}(Either{TLeft, TRight}, Action{TLeft}, Action{TRight})"/>
-	public static Task MatchAsync<TLeft, TRight>(this Task<Either<TLeft, TRight>> @this, Func<TLeft, Task> left, Func<TRight, Task> right) =>
+	/// <inheritdoc cref="E.Match{TEither, TLeft, TRight}(TEither, Action{TLeft}, Action{TRight})"/>
+	public static Task MatchAsync<TEither, TLeft, TRight>(this Task<TEither> @this, Func<TLeft, Task> left, Func<TRight, Task> right)
+		where TEither : IEither<TEither, TLeft, TRight> =>
 		E.MatchAsync(@this, left, right);
 
-	/// <inheritdoc cref="E.Match{TLeft, TRight, TReturn}(Either{TLeft, TRight}, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
-	public static TReturn Match<TLeft, TRight, TReturn>(this Either<TLeft, TRight> @this, Func<TLeft, TReturn> left, Func<TRight, TReturn> right) =>
+	/// <inheritdoc cref="E.Match{TEither, TLeft, TRight, TReturn}(TEither, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
+	public static TReturn Match<TEither, TLeft, TRight, TReturn>(this TEither @this, Func<TLeft, TReturn> left, Func<TRight, TReturn> right)
+		where TEither : IEither<TEither, TLeft, TRight> =>
 		E.Match(@this, left, right);
 
-	/// <inheritdoc cref="E.Match{TLeft, TRight, TReturn}(Either{TLeft, TRight}, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
-	public static Task<TReturn> MatchAsync<TLeft, TRight, TReturn>(this Either<TLeft, TRight> @this, Func<TLeft, TReturn> left, Func<TRight, Task<TReturn>> right) =>
+	/// <inheritdoc cref="E.Match{TEither, TLeft, TRight, TReturn}(TEither, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
+	public static Task<TReturn> MatchAsync<TEither, TLeft, TRight, TReturn>(this TEither @this, Func<TLeft, TReturn> left, Func<TRight, Task<TReturn>> right)
+		where TEither : IEither<TEither, TLeft, TRight> =>
 		E.MatchAsync(@this, left, right);
 
-	/// <inheritdoc cref="E.Match{TLeft, TRight, TReturn}(Either{TLeft, TRight}, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
-	public static Task<TReturn> MatchAsync<TLeft, TRight, TReturn>(this Either<TLeft, TRight> @this, Func<TLeft, Task<TReturn>> left, Func<TRight, TReturn> right) =>
+	/// <inheritdoc cref="E.Match{TEither, TLeft, TRight, TReturn}(TEither, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
+	public static Task<TReturn> MatchAsync<TEither, TLeft, TRight, TReturn>(this TEither @this, Func<TLeft, Task<TReturn>> left, Func<TRight, TReturn> right)
+		where TEither : IEither<TEither, TLeft, TRight> =>
 		E.MatchAsync(@this, left, right);
 
-	/// <inheritdoc cref="E.Match{TLeft, TRight, TReturn}(Either{TLeft, TRight}, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
-	public static Task<TReturn> MatchAsync<TLeft, TRight, TReturn>(this Either<TLeft, TRight> @this, Func<TLeft, Task<TReturn>> left, Func<TRight, Task<TReturn>> right) =>
+	/// <inheritdoc cref="E.Match{TEither, TLeft, TRight, TReturn}(TEither, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
+	public static Task<TReturn> MatchAsync<TEither, TLeft, TRight, TReturn>(this TEither @this, Func<TLeft, Task<TReturn>> left, Func<TRight, Task<TReturn>> right)
+		where TEither : IEither<TEither, TLeft, TRight> =>
 		E.MatchAsync(@this, left, right);
 
-	/// <inheritdoc cref="E.Match{TLeft, TRight, TReturn}(Either{TLeft, TRight}, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
-	public static Task<TReturn> MatchAsync<TLeft, TRight, TReturn>(this Task<Either<TLeft, TRight>> @this, Func<TLeft, TReturn> left, Func<TRight, TReturn> right) =>
+	/// <inheritdoc cref="E.Match{TEither, TLeft, TRight, TReturn}(TEither, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
+	public static Task<TReturn> MatchAsync<TEither, TLeft, TRight, TReturn>(this Task<TEither> @this, Func<TLeft, TReturn> left, Func<TRight, TReturn> right)
+		where TEither : IEither<TEither, TLeft, TRight> =>
 		E.MatchAsync(@this, left, right);
 
-	/// <inheritdoc cref="E.Match{TLeft, TRight, TReturn}(Either{TLeft, TRight}, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
-	public static Task<TReturn> MatchAsync<TLeft, TRight, TReturn>(this Task<Either<TLeft, TRight>> @this, Func<TLeft, TReturn> left, Func<TRight, Task<TReturn>> right) =>
+	/// <inheritdoc cref="E.Match{TEither, TLeft, TRight, TReturn}(TEither, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
+	public static Task<TReturn> MatchAsync<TEither, TLeft, TRight, TReturn>(this Task<TEither> @this, Func<TLeft, TReturn> left, Func<TRight, Task<TReturn>> right)
+		where TEither : IEither<TEither, TLeft, TRight> =>
 		E.MatchAsync(@this, left, right);
 
-	/// <inheritdoc cref="E.Match{TLeft, TRight, TReturn}(Either{TLeft, TRight}, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
-	public static Task<TReturn> MatchAsync<TLeft, TRight, TReturn>(this Task<Either<TLeft, TRight>> @this, Func<TLeft, Task<TReturn>> left, Func<TRight, TReturn> right) =>
+	/// <inheritdoc cref="E.Match{TEither, TLeft, TRight, TReturn}(TEither, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
+	public static Task<TReturn> MatchAsync<TEither, TLeft, TRight, TReturn>(this Task<TEither> @this, Func<TLeft, Task<TReturn>> left, Func<TRight, TReturn> right)
+		where TEither : IEither<TEither, TLeft, TRight> =>
 		E.MatchAsync(@this, left, right);
 
-	/// <inheritdoc cref="E.Match{TLeft, TRight, TReturn}(Either{TLeft, TRight}, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
-	public static Task<TReturn> MatchAsync<TLeft, TRight, TReturn>(this Task<Either<TLeft, TRight>> @this, Func<TLeft, Task<TReturn>> left, Func<TRight, Task<TReturn>> right) =>
+	/// <inheritdoc cref="E.Match{TEither, TLeft, TRight, TReturn}(TEither, Func{TLeft, TReturn}, Func{TRight, TReturn})"/>
+	public static Task<TReturn> MatchAsync<TEither, TLeft, TRight, TReturn>(this Task<TEither> @this, Func<TLeft, Task<TReturn>> left, Func<TRight, Task<TReturn>> right)
+		where TEither : IEither<TEither, TLeft, TRight> =>
 		E.MatchAsync(@this, left, right);
 }
