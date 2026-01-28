b3f4d85687ef5b914dce48877b556199bbf2227f
bfren <bf@bfren.dev>
2026-01-28 18:31:44 +0000

    Adding Nullable tests for Ok<T> - #6

---
+++ tests/Tests.Result/_/Ok/Constructor_Tests.cs
@@ -0,0 +1,71 @@
+// Wrap: Unit Tests.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+namespace Wrap.Ok_Tests;
+
+public class Constructor_Tests
+{
+	public class With_Null
+	{
+		public class Reference_Type
+		{
+			[Fact]
+			public void Throws_ArgumentNullException()
+			{
+				// Arrange
+
+				// Act
+				var result = Record.Exception(() => new Ok<string>(null!));
+
+				// Assert
+				Assert.IsType<ArgumentNullException>(result);
+			}
+		}
+
+		public class Nullable_Reference_Type
+		{
+			[Fact]
+			public void Throws_ArgumentNullException()
+			{
+				// Arrange
+
+				// Act
+				var result = Record.Exception(() => new Ok<string?>(null!));
+
+				// Assert
+				Assert.IsType<ArgumentNullException>(result);
+			}
+		}
+
+		public class Nullable_Value_Type
+		{
+			[Fact]
+			public void Throws_ArgumentNullException()
+			{
+				// Arrange
+
+				// Act
+				var result = Record.Exception(() => new Ok<int?>(null!));
+
+				// Assert
+				Assert.IsType<ArgumentNullException>(result);
+			}
+		}
+	}
+
+	public class With_Value
+	{
+		[Fact]
+		public void Sets_Value_Property()
+		{
+			// Arrange
+			var value = Rnd.UPtr;
+
+			// Act
+			var result = new Ok<nuint>(value);
+
+			// Assert
+			Assert.Equal(value, result.Value);
+		}
+	}
+}
