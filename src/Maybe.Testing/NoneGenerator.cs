97d9f6c4d87b8f925d396adb55d6dcbec663db06
bfren <bf@bfren.dev>
2026-01-29 12:29:36 +0000

    Adding NoneGenerator - #6

---
+++ src/Maybe.Testing/NoneGenerator.cs
@@ -0,0 +1,18 @@
+// Wrap: .NET monads.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+namespace Wrap.Testing;
+
+/// <summary>
+/// Generate strongly-type None values.
+/// </summary>
+public static class NoneGenerator
+{
+	/// <summary>
+	/// Returns a <see cref="None"/> value .
+	/// </summary>
+	/// <typeparam name="T">Some value type.</typeparam>
+	/// <returns>A <see cref="None"/> object implicitly returned as <see cref="Maybe{T}"/>.</returns>
+	public static Maybe<T> Create<T>() =>
+		M.None;
+}
