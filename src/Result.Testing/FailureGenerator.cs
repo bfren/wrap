8e025f7ac89ca4b867bc17289b064d824ba1b527
bfren <bf@bfren.dev>
2026-01-30 14:17:29 +0000

    Generate Failures from Exception objects - #6

--- src/Result.Testing/FailureGenerator.cs
+++ src/Result.Testing/FailureGenerator.cs
@@ -1,6 +1,8 @@
 // Wrap: .NET monads.
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
 
+using System;
+
 namespace Wrap.Testing;
 
 /// <summary>
@@ -30,6 +32,16 @@ public static class FailureGenerator
 	/// <returns>A <see cref="Failure"/> object implicitly returned as <see cref="Result{T}"/>.</returns>
 	public static Result<T> Create<T>() =>
 		new Failure(Value);
+
+	/// <summary>
+	/// Creates a new instance of the <see cref="Failure"/> class with a provided Exception.
+	/// </summary>
+	/// <remarks>Use this method to generate a failure result for testing or simulation purposes. The returned
+	/// instance will have its value set to a randomly generated string.</remarks>
+	/// <param name="ex">Exception.</param>
+	/// <returns>A <see cref="Failure"/> object initialized with a random failure value.</returns>
+	public static Failure Create(Exception ex) =>
+		new(ex);
 
 	/// <summary>
 	/// Creates a new instance of the <see cref="Failure"/> class with the specified failure value.
