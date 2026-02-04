825484d355a2a958e5a42bda9329b70d0a4c0499
bfren <bf@bfren.dev>
2026-02-04 17:58:54 +0000

    Adding FilterBind tests for Maybe and Result - #13

---
+++ tests/Tests.All/Extensions/EnumerableExtensions/FilterBind_Tests1.cs
@@ -0,0 +1,172 @@
+// Wrap: Unit Tests.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+namespace Wrap.Extensions.EnumerableExtensions_Tests;
+
+public partial class FilterBind_Tests
+{
+	private static ResultVars SetupResult(bool predicateReturn, bool withValues, bool mixed = false)
+	{
+		var predicate = Substitute.For<Func<int, bool>>();
+		predicate.Invoke(Arg.Any<int>()).Returns(predicateReturn);
+
+		var bind = Substitute.For<Func<int, Result<string>>>();
+
+		var list = new[] { Rnd.Int, Rnd.Int, Rnd.Int };
+
+		return new(GetResult(withValues ? list : null, mixed), predicate, bind, list);
+	}
+
+	private static IEnumerable<Result<int>> GetResult(int[]? values, bool mixed)
+	{
+		for (var i = 0; i < 3; i++)
+		{
+			if (values is not null)
+			{
+				yield return R.Wrap(values[i]);
+			}
+			else
+			{
+				yield return FailGen.Create();
+			}
+
+			if (mixed)
+			{
+				yield return FailGen.Create();
+			}
+		}
+	}
+
+	public class With_Failure
+	{
+		public class Predicate_Returns_False
+		{
+			[Fact]
+			public void Returns_Failures()
+			{
+				// Arrange
+				var v = SetupResult(false, false);
+
+				// Act
+				var result = v.List.FilterBind(v.Predicate, v.Bind);
+
+				// Assert
+				Assert.Collection(result,
+					x => x.AssertFailure(),
+					x => x.AssertFailure(),
+					x => x.AssertFailure()
+				);
+			}
+
+			[Fact]
+			public void Function_Is_Not_Invoked()
+			{
+				// Arrange
+				var v = SetupResult(false, false);
+
+				// Act
+				_ = v.List.FilterBind(v.Predicate, v.Bind);
+
+				// Assert
+				v.Bind.DidNotReceiveWithAnyArgs().Invoke(Arg.Any<int>());
+			}
+		}
+
+		public class Predicate_Returns_True
+		{
+			[Fact]
+			public void Returns_Failures()
+			{
+				// Arrange
+				var v = SetupResult(true, false);
+
+				// Act
+				var result = v.List.FilterBind(v.Predicate, v.Bind);
+
+				// Assert
+				Assert.Collection(result,
+					x => x.AssertFailure(),
+					x => x.AssertFailure(),
+					x => x.AssertFailure()
+				);
+			}
+
+			[Fact]
+			public void Function_Is_Not_Invoked()
+			{
+				// Arrange
+				var v = SetupResult(true, false);
+
+				// Act
+				_ = v.List.FilterBind(v.Predicate, v.Bind);
+
+				// Assert
+				v.Bind.DidNotReceiveWithAnyArgs().Invoke(Arg.Any<int>());
+			}
+		}
+	}
+
+	public class With_Ok
+	{
+		public class Predicate_Returns_False
+		{
+			[Fact]
+			public void Returns_Failures()
+			{
+				// Arrange
+				var v = SetupResult(false, true);
+
+				// Act
+				var result = v.List.FilterBind(v.Predicate, v.Bind);
+
+				// Assert
+				Assert.Collection(result,
+					x => x.AssertFailure(C.PredicateFalseMessage),
+					x => x.AssertFailure(C.PredicateFalseMessage),
+					x => x.AssertFailure(C.PredicateFalseMessage)
+				);
+			}
+
+			[Fact]
+			public void Function_Is_Not_Invoked()
+			{
+				// Arrange
+				var v = SetupResult(true, true);
+
+				// Act
+				_ = v.List.FilterBind(v.Predicate, v.Bind);
+
+				// Assert
+				v.Bind.DidNotReceiveWithAnyArgs().Invoke(Arg.Any<int>());
+			}
+		}
+
+		public class Predicate_Returns_True
+		{
+			[Fact]
+			public void Returns_Ok_With_Value()
+			{
+				// Arrange
+				var v = SetupResult(true, true);
+				v.Bind.Invoke(Arg.Any<int>()).Returns(c => c.Arg<int>().ToString());
+
+				// Act
+				var result = v.List.FilterBind(v.Predicate, v.Bind);
+
+				// Assert
+				Assert.Collection(result,
+					x => x.AssertOk(v.Values[0].ToString()),
+					x => x.AssertOk(v.Values[1].ToString()),
+					x => x.AssertOk(v.Values[2].ToString())
+				);
+			}
+		}
+	}
+
+	private record class ResultVars(
+		IEnumerable<Result<int>> List,
+		Func<int, bool> Predicate,
+		Func<int, Result<string>> Bind,
+		int[] Values
+	);
+}
