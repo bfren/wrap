9584a7039bcd944d37a68de783b5410280407d3f
bfren <bf@bfren.dev>
2026-02-07 16:08:05 +0000

    Adding Iterate and tests - #13

--- src/All/Extensions/Enumerable/EnumerableExtensions.Iterate.cs
+++ src/All/Extensions/Enumerable/EnumerableExtensions.Iterate.cs
@@ -22,45 +22,71 @@ public static partial class EnumerableExtensions
 	{
 		foreach (var item in @this)
 		{
-			foreach (var some in item)
+			foreach (var value in item)
 			{
-				f(some);
+				f(value);
 			}
 		}
 	}
 
 	/// <inheritdoc cref="Iterate{T}(IEnumerable{Maybe{T}}, Action{T})"/>
-	public static async Task IterateAsync<T>(this IEnumerable<Maybe<T>> @this, Func<T, Task> f)
-	{
-		foreach (var item in @this)
-		{
-			foreach (var some in item)
-			{
-				await f(some);
-			}
-		}
-	}
+	public static Task IterateAsync<T>(this IEnumerable<Maybe<T>> @this, Func<T, Task> f) =>
+		IterateAsync(Task.FromResult(@this), f);
 
 	/// <inheritdoc cref="Iterate{T}(IEnumerable{Maybe{T}}, Action{T})"/>
-	public static async Task IterateAsync<T>(this Task<IEnumerable<Maybe<T>>> @this, Action<T> f)
-	{
-		foreach (var item in await @this)
-		{
-			foreach (var some in item)
-			{
-				f(some);
-			}
-		}
-	}
+	public static Task IterateAsync<T>(this Task<IEnumerable<Maybe<T>>> @this, Action<T> f) =>
+		IterateAsync(@this, x => { f(x); return Task.CompletedTask; });
 
 	/// <inheritdoc cref="Iterate{T}(IEnumerable{Maybe{T}}, Action{T})"/>
 	public static async Task IterateAsync<T>(this Task<IEnumerable<Maybe<T>>> @this, Func<T, Task> f)
 	{
 		foreach (var item in await @this)
 		{
-			foreach (var some in item)
+			foreach (var value in item)
 			{
-				await f(some);
+				await f(value);
+			}
+		}
+	}
+
+	#endregion
+
+	#region Result
+
+	/// <summary>
+	/// Loop through list <paramref name="this"/> and apply function <paramref name="f"/> to each element
+	/// that is a <see cref="Ok{T}"/>.
+	/// </summary>
+	/// <typeparam name="T">Ok value type.</typeparam>
+	/// <param name="this">List of <see cref="Result{T}"/> objects.</param>
+	/// <param name="f">Function to apply to each <see cref="Ok{T}"/> element of <paramref name="this"/>.</param>
+	public static void Iterate<T>(this IEnumerable<Result<T>> @this, Action<T> f)
+	{
+		foreach (var item in @this)
+		{
+			foreach (var value in item.Unsafe())
+			{
+				f(value);
+			}
+		}
+	}
+
+	/// <inheritdoc cref="Iterate{T}(IEnumerable{Result{T}}, Action{T})"/>
+	public static Task IterateAsync<T>(this IEnumerable<Result<T>> @this, Func<T, Task> f) =>
+		IterateAsync(Task.FromResult(@this), f);
+
+	/// <inheritdoc cref="Iterate{T}(IEnumerable{Result{T}}, Action{T})"/>
+	public static Task IterateAsync<T>(this Task<IEnumerable<Result<T>>> @this, Action<T> f) =>
+		IterateAsync(@this, x => { f(x); return Task.CompletedTask; });
+
+	/// <inheritdoc cref="Iterate{T}(IEnumerable{Result{T}}, Action{T})"/>
+	public static async Task IterateAsync<T>(this Task<IEnumerable<Result<T>>> @this, Func<T, Task> f)
+	{
+		foreach (var item in await @this)
+		{
+			foreach (var value in item.Unsafe())
+			{
+				await f(value);
 			}
 		}
 	}
