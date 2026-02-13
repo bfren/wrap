efe70f8d460f3cf1bb996d9dc98b16f84be0c484
bfren <bf@bfren.dev>
2026-02-13 12:57:21 +0000

    Adding All.Mvc project with tests - #41

---
+++ src/All.Mvc/Extensions/List/ListExtensions.AddWrapModelBinders.cs
@@ -0,0 +1,17 @@
+// Wrap: .NET monads.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Wrap.Mvc;
+
+public static partial class ListExtensions
+{
+	/// <summary>
+	/// Insert Wrap Model Binders.
+	/// </summary>
+	/// <param name="this">IModelBinderProvider list.</param>
+	public static void AddWrapModelBinders(this IList<IModelBinderProvider> @this) =>
+		@this.Insert(0, new MonadModelBinderProvider());
+}
---
+++ tests/Tests.All.Mvc/Extensions/ListExtensions/AddWrapModelBinders_Tests.cs
@@ -0,0 +1,22 @@
+// Wrap: Unit Tests.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Wrap.Mvc.ListExtensions_Tests;
+
+public class AddWrapModelBinders_Tests
+{
+	[Fact]
+	public void Inserts_MonadModelBinderProvider()
+	{
+		// Arrange
+		var list = Substitute.For<IList<IModelBinderProvider>>();
+
+		// Act
+		list.AddWrapModelBinders();
+
+		// Assert
+		list.Received().Insert(0, Arg.Any<MonadModelBinderProvider>());
+	}
+}
