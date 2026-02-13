eb06256362de912b63d7c8340488168cf4e78b5b
bfren <bf@bfren.dev>
2026-02-13 10:13:28 +0000

    Moving AddWrapConverters - #40

---
+++ src/All/Json/Extensions/ListExtensions.cs
@@ -0,0 +1,21 @@
+// Wrap: .NET monads.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Wrap.Json;
+
+public static partial class ListExtensions
+{
+	/// <summary>
+	/// Add <see cref="MaybeJsonConverterFactory"/> and
+	/// <see cref="MonadJsonConverterFactory"/> to the list of JSON converters.
+	/// </summary>
+	/// <param name="this">JSON serializer options.</param>
+	public static void AddWrapConverters(this IList<JsonConverter> @this)
+	{
+		@this.Add(new MaybeJsonConverterFactory());
+		@this.Add(new MonadJsonConverterFactory());
+	}
+}
