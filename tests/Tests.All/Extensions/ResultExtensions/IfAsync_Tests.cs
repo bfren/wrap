9c31b143f92f8f3eb00e52616e3cb8c9a6c7e2c2
bfren <bf@bfren.dev>
2026-02-18 21:00:04 +0000

    Vibe-coding If/ Async tests - #53

---
+++ tests/Tests.All/Extensions/ResultExtensions/IfAsync_Tests.cs
@@ -0,0 +1,140 @@
+// Wrap: Unit Tests.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+namespace Wrap.Extensions.ResultExtensions_Tests;
+
+public class IfAsync_Tests
+{
+	private static (Func<string, bool> fTest, Func<string, Result<int>> fTrue, Func<string, Result<int>> fFalse) Setup(bool predicateReturn)
+	{
+		var fTest = Substitute.For<Func<string, bool>>();
+		fTest.Invoke(Arg.Any<string>()).Returns(predicateReturn);
+		var fTrue = Substitute.For<Func<string, Result<int>>>();
+		var fFalse = Substitute.For<Func<string, Result<int>>>();
+		return (fTest, fTrue, fFalse);
+	}
+
+	public class With_Failure
+	{
+		[Fact]
+		public async Task Returns_Failure()
+		{
+			// Arrange
+			var value = Rnd.Str;
+			var input = FailGen.Create<string>(new(value));
+			var (fTest, fTrue, fFalse) = Setup(true);
+
+			// Act
+			var r0 = await input.IfAsync(fTest, x => Task.FromResult(fTrue(x)), x => Task.FromResult(fFalse(x)));
+			var r1 = await input.AsTask().IfAsync(fTest, fTrue, fFalse);
+			var r2 = await input.AsTask().IfAsync(fTest, x => Task.FromResult(fTrue(x)), x => Task.FromResult(fFalse(x)));
+
+			// Assert
+			r0.AssertFailure(value);
+			r1.AssertFailure(value);
+			r2.AssertFailure(value);
+		}
+	}
+
+	public class With_Ok
+	{
+		public class Predicate_Returns_False
+		{
+			[Fact]
+			public async Task Returns_fFalse_Result()
+			{
+				// Arrange
+				var returnValue = Rnd.Int;
+				var input = R.Wrap(Rnd.Str);
+				var (fTest, fTrue, fFalse) = Setup(false);
+				fFalse.Invoke(Arg.Any<string>()).Returns(returnValue);
+
+				// Act
+				var r0 = await input.IfAsync(fTest, x => Task.FromResult(fTrue(x)), x => Task.FromResult(fFalse(x)));
+				var r1 = await input.AsTask().IfAsync(fTest, fTrue, fFalse);
+				var r2 = await input.AsTask().IfAsync(fTest, x => Task.FromResult(fTrue(x)), x => Task.FromResult(fFalse(x)));
+
+				// Assert
+				r0.AssertOk(returnValue);
+				r1.AssertOk(returnValue);
+				r2.AssertOk(returnValue);
+			}
+		}
+
+		public class Predicate_Returns_True
+		{
+			[Fact]
+			public async Task Returns_fTrue_Result()
+			{
+				// Arrange
+				var returnValue = Rnd.Int;
+				var input = R.Wrap(Rnd.Str);
+				var (fTest, fTrue, fFalse) = Setup(true);
+				fTrue.Invoke(Arg.Any<string>()).Returns(returnValue);
+
+				// Act
+				var r0 = await input.IfAsync(fTest, x => Task.FromResult(fTrue(x)), x => Task.FromResult(fFalse(x)));
+				var r1 = await input.AsTask().IfAsync(fTest, fTrue, fFalse);
+				var r2 = await input.AsTask().IfAsync(fTest, x => Task.FromResult(fTrue(x)), x => Task.FromResult(fFalse(x)));
+
+				// Assert
+				r0.AssertOk(returnValue);
+				r1.AssertOk(returnValue);
+				r2.AssertOk(returnValue);
+			}
+		}
+	}
+
+	// Act-if-true special case
+	public class Act_If_True
+	{
+		public class Predicate_Returns_False
+		{
+			[Fact]
+			public async Task Returns_Original_Value()
+			{
+				// Arrange
+				var value = Rnd.Int;
+				var input = R.Wrap(value);
+				var fTest = Substitute.For<Func<int, bool>>();
+				fTest.Invoke(Arg.Any<int>()).Returns(false);
+				var fThen = Substitute.For<Func<int, Task<Result<int>>>>();
+
+				// Act
+				var r0 = await input.IfAsync(fTest, fThen);
+				var r1 = await input.AsTask().IfAsync(fTest, x => fThen(x));
+				var r2 = await input.AsTask().IfAsync(fTest, fThen);
+
+				// Assert
+				r0.AssertOk(value);
+				r1.AssertOk(value);
+				r2.AssertOk(value);
+			}
+		}
+
+		public class Predicate_Returns_True
+		{
+			[Fact]
+			public async Task Returns_fThen_Result()
+			{
+				// Arrange
+				var returnValue = Rnd.Int;
+				var input = R.Wrap(Rnd.Int);
+				var fTest = Substitute.For<Func<int, bool>>();
+				fTest.Invoke(Arg.Any<int>()).Returns(true);
+				var fThen = Substitute.For<Func<int, Task<Result<int>>>>();
+				fThen.Invoke(Arg.Any<int>()).Returns(Task.FromResult<Result<int>>(returnValue));
+
+				// Act
+				var r0 = await input.IfAsync(fTest, fThen);
+				var r1 = await input.AsTask().IfAsync(fTest, x => fThen(x));
+				var r2 = await input.AsTask().IfAsync(fTest, fThen);
+
+				// Assert
+				r0.AssertOk(returnValue);
+				r1.AssertOk(returnValue);
+				r2.AssertOk(returnValue);
+			}
+		}
+	}
+}
---
+++ tests/Tests.All/Extensions/ResultExtensions/IfNot_Tests.cs
@@ -0,0 +1,103 @@
+// Wrap: Unit Tests.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+namespace Wrap.Extensions.ResultExtensions_Tests;
+
+public class IfNot_Tests
+{
+	public class With_Failure
+	{
+		[Fact]
+		public void Returns_Failure()
+		{
+			// Arrange
+			var value = Rnd.Str;
+			var input = FailGen.Create<int>(new(value));
+			var fTest = Substitute.For<Func<int, bool>>();
+			fTest.Invoke(Arg.Any<int>()).Returns(false);
+			var fThen = Substitute.For<Func<int, Result<int>>>();
+
+			// Act
+			var result = input.IfNot(fTest, fThen);
+
+			// Assert
+			result.AssertFailure(value);
+		}
+
+		[Fact]
+		public void fThen_Is_Not_Invoked()
+		{
+			// Arrange
+			var input = FailGen.Create<int>();
+			var fTest = Substitute.For<Func<int, bool>>();
+			fTest.Invoke(Arg.Any<int>()).Returns(false);
+			var fThen = Substitute.For<Func<int, Result<int>>>();
+
+			// Act
+			_ = input.IfNot(fTest, fThen);
+
+			// Assert
+			fThen.DidNotReceiveWithAnyArgs().Invoke(Arg.Any<int>());
+		}
+	}
+
+	public class With_Ok
+	{
+		public class Predicate_Returns_True
+		{
+			[Fact]
+			public void Returns_Original_Value()
+			{
+				// Arrange
+				var value = Rnd.Int;
+				var input = R.Wrap(value);
+				var fTest = Substitute.For<Func<int, bool>>();
+				fTest.Invoke(Arg.Any<int>()).Returns(true);
+				var fThen = Substitute.For<Func<int, Result<int>>>();
+
+				// Act
+				var result = input.IfNot(fTest, fThen);
+
+				// Assert
+				result.AssertOk(value);
+			}
+
+			[Fact]
+			public void fThen_Is_Not_Invoked()
+			{
+				// Arrange
+				var input = R.Wrap(Rnd.Int);
+				var fTest = Substitute.For<Func<int, bool>>();
+				fTest.Invoke(Arg.Any<int>()).Returns(true);
+				var fThen = Substitute.For<Func<int, Result<int>>>();
+
+				// Act
+				_ = input.IfNot(fTest, fThen);
+
+				// Assert
+				fThen.DidNotReceiveWithAnyArgs().Invoke(Arg.Any<int>());
+			}
+		}
+
+		public class Predicate_Returns_False
+		{
+			[Fact]
+			public void Returns_fThen_Result()
+			{
+				// Arrange
+				var returnValue = Rnd.Int;
+				var input = R.Wrap(Rnd.Int);
+				var fTest = Substitute.For<Func<int, bool>>();
+				fTest.Invoke(Arg.Any<int>()).Returns(false);
+				var fThen = Substitute.For<Func<int, Result<int>>>();
+				fThen.Invoke(Arg.Any<int>()).Returns(returnValue);
+
+				// Act
+				var result = input.IfNot(fTest, fThen);
+
+				// Assert
+				result.AssertOk(returnValue);
+			}
+		}
+	}
+}
