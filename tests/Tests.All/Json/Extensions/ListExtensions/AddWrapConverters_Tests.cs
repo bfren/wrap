7dac03f4a020a14059208786bf7148ebb1a18480
bfren <bf@bfren.dev>
2026-02-18 10:32:04 +0000

    Vibe-coding AddWrapConverters tests - #53

---
+++ tests/Tests.All/Json/Extensions/ListExtensions/AddWrapConverters_Tests.cs
@@ -0,0 +1,23 @@
+// Wrap: Unit Tests.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+using System.Text.Json.Serialization;
+
+namespace Wrap.Json.ListExtensions_Tests;
+
+public class AddWrapConverters_Tests
+{
+	[Fact]
+	public void Adds_MaybeJsonConverterFactory_And_MonadJsonConverterFactory()
+	{
+		// Arrange
+		var list = Substitute.For<IList<JsonConverter>>();
+
+		// Act
+		list.AddWrapConverters();
+
+		// Assert
+		list.Received().Add(Arg.Any<MaybeJsonConverterFactory>());
+		list.Received().Add(Arg.Any<MonadJsonConverterFactory>());
+	}
+}
