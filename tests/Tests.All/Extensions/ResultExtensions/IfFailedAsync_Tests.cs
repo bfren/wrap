cd9bc145e481f68611d89cca542032af34bd27fd
bfren <bf@bfren.dev>
2026-02-18 10:38:45 +0000

    Vibe-coding IfOk / IfFailed + Async tests - #53

---
+++ tests/Tests.All/Extensions/ResultExtensions/IfFailedAsync_Tests.cs
@@ -0,0 +1,83 @@
+// Wrap: Unit Tests.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+namespace Wrap.Extensions.ResultExtensions_Tests;
+
+public class IfFailedAsync_Tests
+{
+	public class With_Ok
+	{
+		[Fact]
+		public async Task Returns_Ok()
+		{
+			// Arrange
+			var value = Rnd.Int;
+			var input = R.Wrap(value);
+			var f = Substitute.For<Action<FailureValue>>();
+
+			// Act
+			var r0 = await input.IfFailedAsync(x => { f(x); return Task.CompletedTask; });
+			var r1 = await input.AsTask().IfFailedAsync(f);
+			var r2 = await input.AsTask().IfFailedAsync(x => { f(x); return Task.CompletedTask; });
+
+			// Assert
+			r0.AssertOk(value);
+			r1.AssertOk(value);
+			r2.AssertOk(value);
+		}
+
+		[Fact]
+		public async Task Function_Is_Not_Invoked()
+		{
+			// Arrange
+			var input = R.Wrap(Rnd.Int);
+			var f = Substitute.For<Action<FailureValue>>();
+
+			// Act
+			_ = await input.IfFailedAsync(x => { f(x); return Task.CompletedTask; });
+			_ = await input.AsTask().IfFailedAsync(f);
+			_ = await input.AsTask().IfFailedAsync(x => { f(x); return Task.CompletedTask; });
+
+			// Assert
+			f.DidNotReceiveWithAnyArgs().Invoke(Arg.Any<FailureValue>());
+		}
+	}
+
+	public class With_Failure
+	{
+		[Fact]
+		public async Task Invokes_Function_With_Failure_Value()
+		{
+			// Arrange
+			var input = FailGen.Create<int>();
+			var f = Substitute.For<Action<FailureValue>>();
+
+			// Act
+			_ = await input.IfFailedAsync(x => { f(x); return Task.CompletedTask; });
+			_ = await input.AsTask().IfFailedAsync(f);
+			_ = await input.AsTask().IfFailedAsync(x => { f(x); return Task.CompletedTask; });
+
+			// Assert
+			f.Received(3).Invoke(Arg.Any<FailureValue>());
+		}
+
+		[Fact]
+		public async Task Returns_Failure()
+		{
+			// Arrange
+			var value = Rnd.Str;
+			var input = FailGen.Create<int>(new(value));
+			var f = Substitute.For<Action<FailureValue>>();
+
+			// Act
+			var r0 = await input.IfFailedAsync(x => { f(x); return Task.CompletedTask; });
+			var r1 = await input.AsTask().IfFailedAsync(f);
+			var r2 = await input.AsTask().IfFailedAsync(x => { f(x); return Task.CompletedTask; });
+
+			// Assert
+			r0.AssertFailure(value);
+			r1.AssertFailure(value);
+			r2.AssertFailure(value);
+		}
+	}
+}
---
+++ tests/Tests.All/Extensions/ResultExtensions/IfOk_Tests.cs
@@ -0,0 +1,72 @@
+// Wrap: Unit Tests.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+namespace Wrap.Extensions.ResultExtensions_Tests;
+
+public class IfOk_Tests
+{
+	public class With_Failure
+	{
+		[Fact]
+		public void Returns_Failure()
+		{
+			// Arrange
+			var value = Rnd.Str;
+			var input = FailGen.Create<int>(new(value));
+			var f = Substitute.For<Action<int>>();
+
+			// Act
+			var result = input.IfOk(f);
+
+			// Assert
+			result.AssertFailure(value);
+		}
+
+		[Fact]
+		public void Function_Is_Not_Invoked()
+		{
+			// Arrange
+			var input = FailGen.Create<int>();
+			var f = Substitute.For<Action<int>>();
+
+			// Act
+			_ = input.IfOk(f);
+
+			// Assert
+			f.DidNotReceiveWithAnyArgs().Invoke(Arg.Any<int>());
+		}
+	}
+
+	public class With_Ok
+	{
+		[Fact]
+		public void Invokes_Function_With_Value()
+		{
+			// Arrange
+			var value = Rnd.Int;
+			var input = R.Wrap(value);
+			var f = Substitute.For<Action<int>>();
+
+			// Act
+			_ = input.IfOk(f);
+
+			// Assert
+			f.Received(1).Invoke(value);
+		}
+
+		[Fact]
+		public void Returns_Ok()
+		{
+			// Arrange
+			var value = Rnd.Int;
+			var input = R.Wrap(value);
+			var f = Substitute.For<Action<int>>();
+
+			// Act
+			var result = input.IfOk(f);
+
+			// Assert
+			result.AssertOk(value);
+		}
+	}
+}
