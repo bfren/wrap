d8f31587172d442f81f56db762e81e4fd45a825f
bfren <bf@bfren.dev>
2026-02-20 08:12:09 +0000

    Vibe-coding Wrap tests - #53

---
+++ tests/Tests.Common/Extensions/ObjectExtensions_Wrap_Tests.cs
@@ -0,0 +1,138 @@
+// Wrap: Unit Tests.
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
+
+using Wrap.Extensions;
+
+namespace Wrap.Extensions.ObjectExtensions_Tests;
+
+public class Wrap_Tests
+{
+	public class Single_Value
+	{
+		[Fact]
+		public void Wraps_Value()
+		{
+			// Arrange
+			var value = Rnd.Int;
+
+			// Act
+			var result = value.Wrap();
+
+			// Assert
+			Assert.Equal(value, result.Value);
+		}
+	}
+
+	public class Single_WrapAsync
+	{
+		[Fact]
+		public async Task With_Value_Source_Wraps_Value()
+		{
+			// Arrange
+			var value = Rnd.Int;
+
+			// Act
+			var result = await value.WrapAsync();
+
+			// Assert
+			Assert.Equal(value, result.Value);
+		}
+
+		[Fact]
+		public async Task With_Task_Source_Wraps_Value()
+		{
+			// Arrange
+			var value = Rnd.Int;
+
+			// Act
+			var result = await Task.FromResult(value).WrapAsync();
+
+			// Assert
+			Assert.Equal(value, result.Value);
+		}
+	}
+
+	public class Enumerable
+	{
+		[Fact]
+		public void Wraps_Each_Value()
+		{
+			// Arrange
+			var v0 = Rnd.Int;
+			var v1 = Rnd.Int;
+			var v2 = Rnd.Int;
+			IEnumerable<int> list = [v0, v1, v2];
+
+			// Act
+			var result = list.Wrap();
+
+			// Assert
+			Assert.Collection(result,
+				x => Assert.Equal(v0, x.Value),
+				x => Assert.Equal(v1, x.Value),
+				x => Assert.Equal(v2, x.Value)
+			);
+		}
+
+		[Fact]
+		public void Skips_Null_Items()
+		{
+			// Arrange
+			var v0 = Rnd.Str;
+			var v1 = Rnd.Str;
+			IEnumerable<string?> list = [v0, null, v1, null];
+
+			// Act
+			var result = list.Wrap();
+
+			// Assert
+			Assert.Collection(result,
+				x => Assert.Equal(v0, x.Value),
+				x => Assert.Equal(v1, x.Value)
+			);
+		}
+	}
+
+	public class Enumerable_WrapAsync
+	{
+		[Fact]
+		public async Task With_IEnumerable_Source_Wraps_Each_Value()
+		{
+			// Arrange
+			var v0 = Rnd.Int;
+			var v1 = Rnd.Int;
+			var v2 = Rnd.Int;
+			IEnumerable<int> list = [v0, v1, v2];
+
+			// Act
+			var result = await list.WrapAsync();
+
+			// Assert
+			Assert.Collection(result,
+				x => Assert.Equal(v0, x.Value),
+				x => Assert.Equal(v1, x.Value),
+				x => Assert.Equal(v2, x.Value)
+			);
+		}
+
+		[Fact]
+		public async Task With_Task_Source_Wraps_Each_Value()
+		{
+			// Arrange
+			var v0 = Rnd.Int;
+			var v1 = Rnd.Int;
+			var v2 = Rnd.Int;
+			IEnumerable<int> list = [v0, v1, v2];
+
+			// Act
+			var result = await Task.FromResult(list).WrapAsync();
+
+			// Assert
+			Assert.Collection(result,
+				x => Assert.Equal(v0, x.Value),
+				x => Assert.Equal(v1, x.Value),
+				x => Assert.Equal(v2, x.Value)
+			);
+		}
+	}
+}
