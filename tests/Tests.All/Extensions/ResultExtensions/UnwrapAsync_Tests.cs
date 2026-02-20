0375ef08137cd42b789c24ba9bb6726e488211ec
bfren <bf@bfren.dev>
2026-02-20 10:22:45 +0000

    Adding additional options for UnwrapAsync plus tests - #53

--- tests/Tests.All/Extensions/ResultExtensions/UnwrapAsync_Tests.cs
+++ tests/Tests.All/Extensions/ResultExtensions/UnwrapAsync_Tests.cs
@@ -1,133 +1,156 @@
 // Wrap: Unit Tests.
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2019
 
+using Wrap.Exceptions;
+
 namespace Wrap.Extensions.ResultExtensions_Tests;
 
 public class UnwrapAsync_Tests
 {
-	public class Throw_On_Failure
+	public class Without_Args
 	{
-		[Fact]
-		public async Task With_Failure__Throws_Exception()
+		public class With_Failure
 		{
-			// Arrange
-			var input = FailGen.Create<int>().AsTask();
+			[Fact]
+			public async Task Throws_Exception()
+			{
+				// Arrange
+				var input = FailGen.Create<int>().AsTask();
 
-			// Act / Assert
-			await Assert.ThrowsAnyAsync<Exception>(() => input.UnwrapAsync());
+				// Act
+				var result = await Record.ExceptionAsync(() => input.UnwrapAsync());
+
+				// Assert
+				Assert.IsType<FailureException>(result);
+			}
 		}
 
-		[Fact]
-		public async Task With_Ok__Returns_Value()
+		public class With_Ok
 		{
-			// Arrange
-			var value = Rnd.Int;
-			var input = R.Wrap(value).AsTask();
 
-			// Act
-			var result = await input.UnwrapAsync();
+			[Fact]
+			public async Task Returns_Value()
+			{
+				// Arrange
+				var value = Rnd.Int;
+				var input = R.Wrap(value).AsTask();
 
-			// Assert
-			Assert.Equal(value, result);
+				// Act
+				var result = await input.UnwrapAsync();
+
+				// Assert
+				Assert.Equal(value, result);
+			}
 		}
 	}
 
-	public class Return_Value_On_Failure
+	public class With_IfFailed
 	{
-		[Fact]
-		public async Task With_Failure__Calls_ifFailed()
+		public class With_Failure
 		{
-			// Arrange
-			var defaultValue = Rnd.Int;
-			var input = FailGen.Create<int>();
+			[Fact]
+			public async Task Calls_ifFailed()
+			{
+				// Arrange
+				var defaultValue = Rnd.Int;
+				var input = FailGen.Create<int>();
 
-			// Act
-			var r0 = await input.UnwrapAsync(async _ => defaultValue);
-			var r1 = await input.AsTask().UnwrapAsync(_ => defaultValue);
-			var r2 = await input.AsTask().UnwrapAsync(async _ => defaultValue);
+				// Act
+				var r0 = await input.UnwrapAsync(async _ => defaultValue);
+				var r1 = await input.AsTask().UnwrapAsync(_ => defaultValue);
+				var r2 = await input.AsTask().UnwrapAsync(async _ => defaultValue);
 
-			// Assert
-			Assert.Equal(defaultValue, r0);
-			Assert.Equal(defaultValue, r1);
-			Assert.Equal(defaultValue, r2);
+				// Assert
+				Assert.Equal(defaultValue, r0);
+				Assert.Equal(defaultValue, r1);
+				Assert.Equal(defaultValue, r2);
+			}
 		}
 
-		[Fact]
-		public async Task With_Ok__Returns_Value()
+		public class With_Ok
 		{
-			// Arrange
-			var value = Rnd.Int;
-			var input = R.Wrap(value);
+			[Fact]
+			public async Task Returns_Value()
+			{
+				// Arrange
+				var value = Rnd.Int;
+				var input = R.Wrap(value);
 
-			// Act
-			var r0 = await input.UnwrapAsync(async _ => Rnd.Int);
-			var r1 = await input.AsTask().UnwrapAsync(_ => Rnd.Int);
-			var r2 = await input.AsTask().UnwrapAsync(async _ => Rnd.Int);
+				// Act
+				var r0 = await input.UnwrapAsync(async _ => Rnd.Int);
+				var r1 = await input.AsTask().UnwrapAsync(_ => Rnd.Int);
+				var r2 = await input.AsTask().UnwrapAsync(async _ => Rnd.Int);
 
-			// Assert
-			Assert.Equal(value, r0);
-			Assert.Equal(value, r1);
-			Assert.Equal(value, r2);
+				// Assert
+				Assert.Equal(value, r0);
+				Assert.Equal(value, r1);
+				Assert.Equal(value, r2);
+			}
 		}
 	}
 
-	public class Handle_Failure_Then_Return
+	public class With_Handler
 	{
-		[Fact]
-		public async Task With_Failure__Calls_ifFailed_Then_Returns_returnThis()
+		public class With_Failure
 		{
-			// Arrange
-			var defaultValue = Rnd.Int;
-			var handlerCalled = false;
-			var input = FailGen.Create<int>();
+			[Fact]
+			public async Task Calls_ifFailed_Then_Returns_returnThis()
+			{
+				// Arrange
+				var value = Rnd.Int;
+				var handler = Substitute.For<Action<FailureValue>>();
+				var input = FailGen.Create<int>();
 
-			// Act
-			var r0 = await input.UnwrapAsync(
-				ifFailed: _ => { handlerCalled = true; return Task.CompletedTask; },
-				returnThis: () => defaultValue
-			);
+				// Act
+				var r0 = await input.UnwrapAsync(async f => handler(f), async () => value);
+				var r1 = await input.UnwrapAsync(async f => handler(f), () => value);
+				var r2 = await input.UnwrapAsync(handler, async () => value);
+				var r3 = await input.AsTask().UnwrapAsync(handler, () => value);
+				var r4 = await input.AsTask().UnwrapAsync(async f => handler(f), () => value);
+				var r5 = await input.AsTask().UnwrapAsync(handler, async () => value);
+				var r6 = await input.AsTask().UnwrapAsync(async f => handler(f), async () => value);
 
-			// Assert
-			Assert.True(handlerCalled);
-			Assert.Equal(defaultValue, r0);
+				// Assert
+				handler.ReceivedWithAnyArgs().Invoke(Arg.Any<FailureValue>());
+				Assert.Equal(value, r0);
+				Assert.Equal(value, r1);
+				Assert.Equal(value, r2);
+				Assert.Equal(value, r3);
+				Assert.Equal(value, r4);
+				Assert.Equal(value, r5);
+				Assert.Equal(value, r6);
+			}
 		}
 
-		[Fact]
-		public async Task With_Failure__Calls_ifFailed_Then_Returns_returnThis_Async()
+		public class With_Ok
 		{
-			// Arrange
-			var defaultValue = Rnd.Int;
-			var handlerCalled = false;
-			var input = FailGen.Create<int>();
+			[Fact]
+			public async Task Returns_Value()
+			{
+				// Arrange
+				var value = Rnd.Int;
+				var handler = Substitute.For<Action<FailureValue>>();
+				var input = R.Wrap(value);
 
-			// Act
-			var r0 = await input.UnwrapAsync(
-				ifFailed: _ => { handlerCalled = true; return Task.CompletedTask; },
-				returnThis: async () => defaultValue
-			);
+				// Act
+				var r0 = await input.UnwrapAsync(async f => handler(f), async () => value);
+				var r1 = await input.UnwrapAsync(async f => handler(f), () => value);
+				var r2 = await input.UnwrapAsync(handler, async () => value);
+				var r3 = await input.AsTask().UnwrapAsync(handler, () => value);
+				var r4 = await input.AsTask().UnwrapAsync(async f => handler(f), () => value);
+				var r5 = await input.AsTask().UnwrapAsync(handler, async () => value);
+				var r6 = await input.AsTask().UnwrapAsync(async f => handler(f), async () => value);
 
-			// Assert
-			Assert.True(handlerCalled);
-			Assert.Equal(defaultValue, r0);
-		}
-
-		[Fact]
-		public async Task With_Ok__Returns_Value()
-		{
-			// Arrange
-			var value = Rnd.Int;
-			var handlerCalled = false;
-			var input = R.Wrap(value);
-
-			// Act
-			var r0 = await input.UnwrapAsync(
-				ifFailed: _ => { handlerCalled = true; return Task.CompletedTask; },
-				returnThis: () => Rnd.Int
-			);
-
-			// Assert
-			Assert.False(handlerCalled);
-			Assert.Equal(value, r0);
+				// Assert
+				handler.DidNotReceiveWithAnyArgs().Invoke(Arg.Any<FailureValue>());
+				Assert.Equal(value, r0);
+				Assert.Equal(value, r1);
+				Assert.Equal(value, r2);
+				Assert.Equal(value, r3);
+				Assert.Equal(value, r4);
+				Assert.Equal(value, r5);
+				Assert.Equal(value, r6);
+			}
 		}
 	}
 }
