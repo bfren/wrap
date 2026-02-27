44dffbe81b8dd194433b2f79d9ea5d69058ec61a
bfren <bf@bfren.dev>
2026-02-27 09:26:16 +0000

    Shifting parse to separate function - #61

--- src/All.Mvc/WrapModelBinder.cs
+++ src/All.Mvc/WrapModelBinder.cs
@@ -82,78 +82,8 @@ public abstract class WrapModelBinder<TValue> : IWrapModelBinder<TValue>
 
 		try
 		{
-			// Attempt to parse the value
-			var valueParsed = default(TValue) switch
-			{
-				bool =>
-					M.ParseBool(value).Match(FNone, FSome),
-
-				byte =>
-					M.ParseByte(value).Match(FNone, FSome),
-
-				char =>
-					M.ParseChar(value).Match(FNone, FSome),
-
-				DateOnly =>
-					M.ParseDateOnly(value).Match(FNone, FSome),
-
-				DateTime =>
-					M.ParseDateTime(value).Match(FNone, FSome),
-
-				DateTimeOffset =>
-					M.ParseDateTimeOffset(value).Match(FNone, FSome),
-
-				decimal =>
-					M.ParseDecimal(value).Match(FNone, FSome),
-
-				double =>
-					M.ParseDouble(value).Match(FNone, FSome),
-
-				Guid =>
-					M.ParseGuid(value).Match(FNone, FSome),
-
-				short =>
-					M.ParseInt16(value).Match(FNone, FSome),
-
-				int =>
-					M.ParseInt32(value).Match(FNone, FSome),
-
-				long =>
-					M.ParseInt64(value).Match(FNone, FSome),
-
-				Int128 =>
-					M.ParseInt128(value).Match(FNone, FSome),
-
-				nint =>
-					M.ParseIntPtr(value).Match(FNone, FSome),
-
-				float =>
-					M.ParseSingle(value).Match(FNone, FSome),
-
-				TimeOnly =>
-					M.ParseTimeOnly(value).Match(FNone, FSome),
-
-				ushort =>
-					M.ParseUInt16(value).Match(FNone, FSome),
-
-				uint =>
-					M.ParseUInt32(value).Match(FNone, FSome),
-
-				ulong =>
-					M.ParseUInt64(value).Match(FNone, FSome),
-
-				UInt128 =>
-					M.ParseUInt128(value).Match(FNone, FSome),
-
-				nuint =>
-					M.ParseUIntPtr(value).Match(FNone, FSome),
-
-				_ =>
-					null
-			};
-
-			// Wrap parsed value
-			return valueParsed switch
+			// Attempt to parse value and wrap result
+			return Parse(value) switch
 			{
 				TValue x =>
 					(result, ModelBindingResult.Success(Wrap(x))),
@@ -173,6 +103,81 @@ public abstract class WrapModelBinder<TValue> : IWrapModelBinder<TValue>
 			return Nothing();
 		}
 	}
+
+	/// <summary>
+	/// Attempt to parse <paramref name="value"/>, returning null on failure.
+	/// </summary>
+	/// <param name="value">String value.</param>
+	/// <returns>Parsed value or null.</returns>
+	internal object? Parse(string? value) =>
+		default(TValue) switch
+		{
+			bool =>
+				M.ParseBool(value).Match(FNone, FSome),
+
+			byte =>
+				M.ParseByte(value).Match(FNone, FSome),
+
+			char =>
+				M.ParseChar(value).Match(FNone, FSome),
+
+			DateOnly =>
+				M.ParseDateOnly(value).Match(FNone, FSome),
+
+			DateTime =>
+				M.ParseDateTime(value).Match(FNone, FSome),
+
+			DateTimeOffset =>
+				M.ParseDateTimeOffset(value).Match(FNone, FSome),
+
+			decimal =>
+				M.ParseDecimal(value).Match(FNone, FSome),
+
+			double =>
+				M.ParseDouble(value).Match(FNone, FSome),
+
+			Guid =>
+				M.ParseGuid(value).Match(FNone, FSome),
+
+			short =>
+				M.ParseInt16(value).Match(FNone, FSome),
+
+			int =>
+				M.ParseInt32(value).Match(FNone, FSome),
+
+			long =>
+				M.ParseInt64(value).Match(FNone, FSome),
+
+			Int128 =>
+				M.ParseInt128(value).Match(FNone, FSome),
+
+			nint =>
+				M.ParseIntPtr(value).Match(FNone, FSome),
+
+			float =>
+				M.ParseSingle(value).Match(FNone, FSome),
+
+			TimeOnly =>
+				M.ParseTimeOnly(value).Match(FNone, FSome),
+
+			ushort =>
+				M.ParseUInt16(value).Match(FNone, FSome),
+
+			uint =>
+				M.ParseUInt32(value).Match(FNone, FSome),
+
+			ulong =>
+				M.ParseUInt64(value).Match(FNone, FSome),
+
+			UInt128 =>
+				M.ParseUInt128(value).Match(FNone, FSome),
+
+			nuint =>
+				M.ParseUIntPtr(value).Match(FNone, FSome),
+
+			_ =>
+				null
+		};
 
 	/// <summary>
 	/// Return a 'None' or null value.
